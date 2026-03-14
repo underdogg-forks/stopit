9e59231d42c3cb847076001e1607e9107561a775
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2026-03-14 04:35:18 +0100

    Delete Modules/Test directory

--- Modules/Test/Routes/Api/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Test\Http\Controllers\TestController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('Tests', TestController::class)->names('test');
-});
--- Modules/Test/Routes/Web/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Test\Http\Controllers\TestController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('Tests', TestController::class)->names('test');
-});
